f053908551b3d05d92e954acf71e23c2420b7670
wizjdan <pangestunugraha1@gmail.com>
2025-01-13 14:08:21 +0700

    Menambahkan controller login dan registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,7 +5,16 @@ class Auth extends CI_Controller {
 
     public function index()
     {
+        $this->load->view("templates/auth_header");
         $this->load->view("auth/login");
+        $this->load->view("templates/auth_footer");
+    }
+
+    public function registration()
+    {
+        $this->load->view("templates/auth_header");
+        $this->load->view("auth/registration");
+        $this->load->view("templates/auth_footer");
     }
 
 }
