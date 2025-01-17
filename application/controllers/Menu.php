6ffb6e349ea6cec6ea04ff7806ce36142d481905
wizjdan <pangestunugraha1@gmail.com>
2025-01-17 14:18:55 +0700

    Memperaiki Modal pada Add New menu, untuk badges nya belum di tambahkan fungsi apapun

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -13,13 +13,27 @@ class Menu extends CI_Controller
 
         //Karena banyak menggunakan result_array()
         $data["menu"] = $this->db->get("user_menu")->result_array();
+
+        $this->form_validation->set_rules("menu", "Menu", "require")
+
+        if( $this->form_validation->run() == false ) {
+
+            $this->load->view("templates/header", $data);
+            $this->load->view("templates/sidebar", $data);
+            $this->load->view("templates/topbar", $data);
+            $this->load->view("menu/index", $data);
+            $this->load->view("templates/footer");
+        } else {
+            //Jika berhasil akan menambahkan data baru
+            $this->db->insert("user_menu", ["menu" => $this->input->post("menu")]);
+
+            $this->session->set_flashdata("messege", '<div class="alert alert-success" role="alert">New mwnu added!</div');
+
+            //Arahkan ke controller menu
+            redirect("menu");
+        }
         
-        $this->load->view("templates/header", $data);
-        $this->load->view("templates/sidebar", $data);
-        $this->load->view("templates/topbar", $data);
-        $this->load->view("menu/index", $data);
-        $this->load->view("templates/footer");
     }
-    
+
 
 }
--- application/views/menu/index.php
+++ application/views/menu/index.php
@@ -10,10 +10,17 @@
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+                    
                     <div class="row">
                         <div class="col-lg-6">
 
-                            <a href="" class="btn btn-primary mb-3">Add New Menu</a>
+                            <!-- Jika eror -->
+                            <?= form_error("menu", '<div class="alert alert-danger" role="alert">','</div'); ?>
+
+                            <!-- Jika success -->
+                            <?= $this->session->flashdata("messege"); ?>
+
+                            <a href="" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#newMenuModal">Add New Menu</a>
 
                             <table class="table table-hover">
                                 <thead>
@@ -49,4 +56,30 @@
             </div>
             <!-- End of Main Content -->
 
-            
+<!-- Modal -->
+<div class="modal fade" id="newMenuModal" tabindex="-1" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newMenuModalLabel">Add New Menu</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="<?= base_url("menu") ?>" method="post">
+          <div class="modal-body">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput menu" name="menu" placeholder="Menu name">
+                <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Add</button>
+        </div>
+    </form>
+    </div>
+  </div>
+</div>
