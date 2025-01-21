5f14c9d9dab9a681ee2780de9ba3c242a8f8c713
wizjdan <pangestunugraha1@gmail.com>
2025-01-21 12:25:50 +0700

    Memperbaiki fitur Change Password serta memberikan flshdata pada masing-masing input

--- application/views/user/changepassword.php
+++ application/views/user/changepassword.php
@@ -10,21 +10,27 @@
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-                    <div class="row">
-                        <div class="col-lg-6">
+                        
+                        <div class="row">
+                            <div class="col-lg-6">
+
+                            <?= $this->session->flashdata("message"); ?>
 
                             <form action="<?= base_url("user/changepassword") ?>" method="post">
                                 <div class="mb-3">
                                     <label for="current_password" class="form-label">Current Password</label>
                                     <input type="password" class="form-control" id="current_password" name="current_password">
+                                    <?= form_error("current_password", '<small class="text-danger">', "</small>"); ?>
                                 </div>
                                 <div class="mb-3">
                                     <label for="new_password1" class="form-label">New Password</label>
                                     <input type="password" class="form-control" id="new_password1" name="new_password1">
+                                    <?= form_error("new_password1", '<small class="text-danger">', "</small>"); ?>
                                 </div>
                                 <div class="mb-3">
                                     <label for="new_password2" class="form-label">Repeat Password</label>
                                     <input type="password" class="form-control" id="new_password2" name="new_password2">
+                                    <?= form_error("new_password2", '<small class="text-danger">', "</small>"); ?>
                                 </div>
                                 <div class="form-group">
                                     <button type="submit" class="btn btn-primary">Change Password</button>
